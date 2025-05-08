417d99a22e554b7daf398e70220358a911df9630
Rez-Ze <marco.croce97@web.de>
2025-05-08 11:10:41 +0200

    Refactor send_bit_task to improve mutex handling and update task initialization

--- src/logic.c
+++ src/logic.c
@@ -1,24 +1,29 @@
 #include <linux/module.h>
 #include <rtai.h>
 #include <rtai_sched.h>
+#include <rtai_sem.h>
+
 
 #define BIT_1_TIME 58000  /* 58 microseconds*/
 #define BIT_0_TIME 100000 /* 100 microsecdons*/
 
 #define STACK_SIZE 4096
-#define LPT1 0x378 /*Pin des Parallelports*/
+#define LPT1 0x378 /*Pin of parallelport*/
 
-RT_TASK mytask;
+SEM mutex;
 
-uint64_t message = 0x5555555555555555;
+RT_TASK msg_task;
+
+uint64_t message = 0xFFFC066230C00000;//0x5555555555555555;
 int length = 42;
 
-static void send_bit_task(void/*uint64_t message, int length*/)
+static void send_bit_task(long n)
 {
   int i;
+  rt_mutex_lock(&mutex);
   for (i = 0; i < length; i++)
   {
-    if ((message >> (63 - i)) & 0x01 == 1)
+    if (((message >> (63 - i)) & 0x01) == 1)
     {
       outb(0x00, LPT1);
       rt_sleep(nano2count(BIT_1_TIME));
@@ -33,18 +38,22 @@ static void send_bit_task(void/*uint64_t message, int length*/)
       rt_sleep(nano2count(BIT_0_TIME));
     }
   }
+  rt_mutex_unlock(&mutex)
 }
 
 static __init int send_init(void)
 {
   rt_mount();
 
-  rt_task_init(&mytask, send_bit_task, 0, STACK_SIZE, 1, 0, 0);
+  rt_mutex_init(&mutex);
+  rt_task_init(&msg_task, send_bit_task, 0, STACK_SIZE, 1, 0, 0);
 
-  rt_set_oneshot_mode();
-  start_rt_timer(0);
+  rt_set_periodic_mode();
+  start_rt_timer(nano2count(20000));
+  
+  rt_task_make_periodic(&msg_task, rt_get_time() + nano2count(1000000), nano2count(20000))
 
-  rt_task_resume(&mytask);
+  //rt_task_resume(&msg_task);
 
   rt_printk("Module loaded\n");
 
@@ -55,7 +64,8 @@ static __exit void send_exit(void)
 {
   stop_rt_timer();
 
-  rt_task_delete(&mytask);
+  rt_task_delete(&msg_task);
+  rt_mutex_delete(&mutex)
 
   rt_umount();
   rt_printk("Unloading module\n");
