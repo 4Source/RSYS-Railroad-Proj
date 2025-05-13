b3501c1c4b5cc86feb2321130fee3d1c243c0247
4Source <38220764+4Source@users.noreply.github.com>
2025-05-13 09:09:42 +0200

    Refine comments in LocomotiveData and MagneticData structures for clarity on bit field values (#3)

--- include/telegram/magnetic.h
+++ include/telegram/magnetic.h
@@ -16,9 +16,9 @@
  */
 typedef struct
 {
-    uint16_t enable : 1;   // [bit 0]        Enables or disables the magnetic device. Values: 0, 1.
-    uint16_t device : 2;   // [bit 1 - 2]    Device type identifier. Values: 0 - 3.
-    uint16_t control : 1;  // [bit 3]        Control status of the device. Values: 0, 1.
+    uint16_t enable : 1;   // [bit 0]        Enables or disables the magnetic device. Values: 0 (off), 1 (on).
+    uint16_t device : 2;   // [bit 1 - 2]    Device type identifier. Values: 0 - 3 (devices 1 - 4).
+    uint16_t control : 1;  // [bit 3]        Control status of the device. Values: 0 (off), 1 (on).
     uint16_t address : 9;  // [bit 4 - 12]   9-bit address of the device. Values: 0 - 511 (0x1FF).
     uint16_t reserved : 3; // [bit 13 - 15]  Reserved for future use.
 } MagneticData;
