25801b2c55d192dd120791309e1d4e9c097e76d7
4Source <my.log.in.dennis@gmail.com>
2025-06-02 18:03:56 +0200

    Fix array declarations for locomotive and magnetic message queues to specify sizes

--- include/communication/railroad_communication.h
+++ include/communication/railroad_communication.h
@@ -21,8 +21,10 @@ uint64_t message = 0xFFFC066230C00000; // 0x5555555555555555;
 int length = 42;
 const int locomotive_count = 3;
 int magnetic_msg_count = 0;
-LocomotiveData locomotive_msg_queue[] = {}; // 0b111111111111110000000110011111110011111001<<22 | {.address = 00000011, .light = 1, .direction = 1, .speed = 110}
-MagneticData magnetic_msg_queue[] = {};
+LocomotiveData locomotive_msg_queue[3] = {}; // 0b111111111111110000000110011111110011111001<<22 | {.address = 00000011, .light = 1, .direction = 1, .speed = 110}
+SEM locomotive_data_mutex[3];
+MagneticData magnetic_msg_queue[4] = {};
+SEM magnetic_data_mutex[4];
 static int loco_sent_count = 0; // Zählt gesendete Lokomotiv-Telegramme
 
 static void send_bit_task(uint64_t message, int length);
